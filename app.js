6ed08b541b8708161a85a939900016b6b51105d8
Elroy Anaki <elroy.a1912@gmail.com>
2024-11-29 02:29:23 +0200

    set the env + Monog connection

---
+++ app.js
@@ -0,0 +1,22 @@
+// Config the env and DB conncetion
+import { config } from "dotenv";
+import { connectToMongoDB } from "./config/DB.config.js";
+config();
+connectToMongoDB();
+
+// Define the server with express library
+import express from "express";
+const app = express();
+const port = Number(process.env.PORT) || 3000;
+
+// Global Middlewares (imports + use)
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes (imports + use)
+
+app.listen(port, () => console.log(`Server running on port ${port}...`));
