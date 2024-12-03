bb824c27fb23f8be5927ebe48ccd75bf641d8939
Israel Fertig <m583288718@gmail.com>
2024-12-03 18:30:23 +0200

    Create a database user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,23 +1,21 @@
-import User from '../models/user.model.js'
+import User from "../models/user.model.js";
 
 export default {
-    signUp: async (req,res)=>{
-        try{
-            console.log(req.body)
-
-        }
-        catch(error){
-            console.log(error);
-            
-
-        }
-
+  signUp: async (req, res) => {
+    try {
+      console.log(req.body);
+      const user = await User.create(req.body);
+      console.log(user);
+      return res
+        .status(200)
+        .json({ success: true, masseage: "success add user", user });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        masseage: "not success add user",
+        error,
+      });
     }
-        
-    
-        
-
-        
-    
-
-}
+  },
+};
