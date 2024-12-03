4cec3144d26cf3adb9d7111c8690f1c29240672e
Elroy Anaki <elroy.a1912@gmail.com>
2024-12-04 00:57:40 +0200

    add multer

---
+++ server/config/multer.config.js
@@ -0,0 +1,27 @@
+import multer, { diskStorage } from "multer";
+
+const storage = diskStorage({
+    destination: "public/uploads",
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Math.round(Math.random() * 1E9)
+        cb(null, uniqueSuffix + '-' + file.originalname)
+    }
+});
+
+function fileFilter(req, file, cb) {
+    if (file.mimetype === "image/jpeg"
+        || file.mimetype === "image/jpg"
+        || file.mimetype === "image/png") {
+        cb(null, true)
+    } else cb(null, false)
+}
+
+
+const upload = multer({
+    storage, fileFilter, limits: {
+        fileSize: 100000
+    }
+});
+
+
+export default upload;
