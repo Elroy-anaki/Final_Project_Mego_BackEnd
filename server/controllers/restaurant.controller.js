29e14f5903f9c14c23d5b7328f6447ca261cee92
Elroy Anaki <elroy.a1912@gmail.com>
2024-12-18 14:30:54 +0200

    test

--- server/controllers/restaurant.controller.js
+++ server/controllers/restaurant.controller.js
@@ -1,29 +1,7 @@
 import Restaurant from '../models/restaurant.model.js';
 import cloudinary from '../config/cloudinary.config.js';
+import {createAvailabilityPipeline} from '../utils/restaurant.js'
 
-function createAvailabilityPipeline(date, time) {
-    return [
-        {
-            $unwind: "$restaurantOccupancyTime"
-        },
-        {
-            $unwind: "$restaurantOccupancyTime.slots"
-        },
-        {
-            $match: {
-                "restaurantOccupancyTime.date": date,         
-                "restaurantOccupancyTime.slots.time": time,    
-            }
-        },
-        {
-            $project: {
-                _id: 0,                                        
-                time: "$restaurantOccupancyTime.slots.time",     
-                remaining: "$restaurantOccupancyTime.slots.remaining" 
-            }
-        }
-    ];
-};
 
 export const addRestaurant = async (req, res) => {
     try {
@@ -63,7 +41,6 @@ export const addRestaurant = async (req, res) => {
 export const getRestaurant = async (req, res) => {
     try {
         const [restaurant] = await Restaurant.find()
-
         res.status(200).json({
             success: true,
             msg: 'Restaurant is here!',
@@ -75,18 +52,14 @@ export const getRestaurant = async (req, res) => {
             success: false,
             msg: 'Not found restaurant!',
             error: error
-
         })
-
     }
-
 };
 
 export const getRemainingSeats = async (req, res) => {
     const { date, time } = req.body;
     console.log(date, time)
     try {
-
         if (!date || !time) throw new Error("Enter date and time")
         const pipeline = createAvailabilityPipeline(date, time)
         const remainingSeats = await Restaurant.aggregate(pipeline)
@@ -111,71 +84,5 @@ export const getRemainingSeats = async (req, res) => {
             msg: error.message || "Enter date and time"
         })
     }
+};
 
-}
-
-
-
-
-
-
-
-
-
-
-// async function isDateExist(date) {
-//     try {
-//         const resultByDate = await Restaurant.findOne({ "restaurantOccupancyTime.date": date }, { "restaurantOccupancyTime.$": 1 });
-//         console.log("resultByDate", resultByDate)
-//         if (resultByDate === null) return false;
-//         else return resultByDate
-//     } catch (error) {
-//         throw error
-//     }
-
-// }
-
-// async function checkRestaurantCapacity(hour, data) {
-//     try {
-//         console.log("DATA", data.restaurantOccupancyTime[0].slots)
-//         const matchingSlots = data.restaurantOccupancyTime[0].slots.filter((slot) => {
-//             return slot.time === hour && slot.remaining < 50;
-//         });
-//         console.log("hourResult", matchingSlots)
-//         return matchingSlots
-//     } catch (error) {
-//         throw error
-
-//     }
-// }
-//export const getFreeTimes = async (req, res) => {
-    //     const { people, date, hour } = req.body;
-    //     console.log(people)
-    //     console.log(date)
-    //     console.log(hour)
-    //     try {
-    //         const dateExist = await isDateExist(date)
-    //         console.log("dateExist", dateExist)
-    //         if (!dateExist) {
-    //             res.status(200).json({
-    //                 success: true,
-    //                 msg: "All hours avalible"
-    //             })
-    //         }
-    //         const hours = await checkRestaurantCapacity(hour, dateExist)
-    //         console.log("hours", hours)
-    //         // const hourResult = resultByDate.restaurantOccupancyTime[0].slots.some((slot) => slot.time === hour && slot.remaining < 50 )
-    //         // console.log(hourResult)
-    //         res.send("Fff")
-    
-    //     } catch (error) {
-    //         console.log("error", error)
-    //         res.send("No")
-    
-    //     }
-    
-    
-    
-    
-    // }
-    
