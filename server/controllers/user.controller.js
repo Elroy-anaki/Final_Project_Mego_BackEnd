9722f1652bfff685d22546aa4c726c845956d142
Israel Fertig <m583288718@gmail.com>
2025-01-05 01:06:58 +0200

    Add Google sign-up functionality and improve user controller error handling

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,56 +1,71 @@
 import User from "../models/user.model.js";
 import { compare, hash } from "bcrypt";
 import { jwtCookieOptions, generateToken } from "../service/auth.service.js";
-import {sendEmailVerification}  from "../service/mail.service.js";
+import { sendEmailVerification } from "../service/mail.service.js";
 
-export const getAllUsers = async (req,res)=>{
-  try{
+
+export const getAllUsers = async (req, res) => {
+  try {
     const users = await User.find();
     res.status(200).json({
       success: true,
       msg: "Users retrieved successfully.",
-      data: users
-      
+      data: users,
     });
-
-  }
-  catch(error){
+  } catch (error) {
     console.log(error);
     res.status(500).json({
       success: false,
       msg: "Failed to retrieve users.",
       error: error.message || error,
     });
-    
   }
+};
 
-}
-
-export const signUp = async (req, res) => {
+export const signUpWithGoogle = async (req, res) => {
   try {
     console.log(req.body);
+    
     const user = await User.create(req.body);
-     sendEmailVerification(user)
-     res
+    
+
+    res
       .status(201)
       .json({ success: true, msg: "success add user", data: user });
   } catch (error) {
     console.log(error.code);
-     res.status(500).json({
+    res.status(500).json({
       success: false,
-      msg: error.code === 11000 ? 'This email exist ' : "Sign Up Failed",
+      msg: error.code === 11000 ? "This email exist " : "Sign Up Failed",
+      error,
+    });
+  }
+};
+export const signUp = async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await User.create(req.body);
+    sendEmailVerification(user);
+
+    res
+      .status(201)
+      .json({ success: true, msg: "success add user", data: user });
+  } catch (error) {
+    console.log(error.code);
+    res.status(500).json({
+      success: false,
+      msg: error.code === 11000 ? "This email exist " : "Sign Up Failed",
       error,
     });
   }
 };
 
 export const signIn = async (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   try {
     const { userPassword, userEmail } = req.body;
 
-    if (!userPassword || !userEmail)
-      throw new Error("all fields required!");
+    if (!userPassword || !userEmail) throw new Error("all fields required!");
 
     const user = await User.findOne({ userEmail });
 
@@ -63,48 +78,49 @@ export const signIn = async (req, res) => {
     const data = generateToken(user);
     const token = data.token;
     const payload = data.payload;
- 
 
     res.cookie("token", token, jwtCookieOptions);
     res.status(200).json({
       success: true,
       msg: "User Sign-in Successfully ",
-      data: payload
+      data: payload,
     });
-
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(401).json({
       success: false,
       msg: error.message,
-      error: error.message || error
+      error: error.message || error,
     });
   }
 };
 
 export const editUserDetails = async (req, res) => {
-  console.log(req.body.userPassword)
+  console.log(req.body.userPassword);
 
-  !req.body.userPassword  ? delete req.body.userPassword : req.body.userPassword = await hash(req.body.userPassword,10);
-  console.log(req.body)
-
+  !req.body.userPassword
+    ? delete req.body.userPassword
+    : (req.body.userPassword = await hash(req.body.userPassword, 10));
+  console.log(req.body);
 
   try {
-    const editedUser = await User.findByIdAndUpdate(req.params.id, {...req.body}, {new: true})
-    
+    const editedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body },
+      { new: true }
+    );
+
     res.status(200).json({
       success: true,
       msg: "Your Changes Saved!",
-      data: editedUser
-    })
+      data: editedUser,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(401).json({
       success: false,
       msg: error.message,
-      error: error.message || error
+      error: error.message || error,
     });
-    
   }
-}
-
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { signUp, signIn, editUserDetails ,getAllUsers} from '../controllers/user.controller.js';
+import { signUp, signIn, editUserDetails ,getAllUsers, signUpWithGoogle} from '../controllers/user.controller.js';
 
 const router = Router();
 
 router.get('/get-all-users', getAllUsers);
 
 router.post('/sign-up', signUp);
+
+router.post('/sign-up/:provider', signUpWithGoogle);
 
 router.post("/sign-in", signIn);
 
