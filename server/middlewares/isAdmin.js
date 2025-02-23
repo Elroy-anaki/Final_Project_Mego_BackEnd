74c2f29e9849037f8a634952efae9e137a2d512f
Elroy Anaki <elroy.a1912@gmail.com>
2025-02-23 02:13:46 +0200

    fix logs

--- server/middlewares/isAdmin.js
+++ server/middlewares/isAdmin.js
@@ -14,7 +14,7 @@ export const isAdmin = async (req, res, next) =>  {
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         console.log("decoded", decoded)
-        console.log("pre", payload.premission)
+        console.log("pre", decoded.payload.premission)
         console.log("JWT", process.env.JWT_SECRET)
 
         if(decoded.payload.premission !== 'admin'){
